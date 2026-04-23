414ebc7a0fae10a1b039ec3f528f47ad2fc9010b
Marcos32112 <marcosantonio344r@gmail.com>
2026-04-23 10:55:06 -0400

    Add files via upload

--- src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
+++ src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
@@ -16,6 +16,7 @@ public class ImageServiceImpl implements ImageService {
     @Override
     @Transactional
     public Image save(Image image){
-        return null;
+
+        return repository.save(image);
     }
 }
--- src/main/java/io/spring/image/demo/application/ImagesController.java
+++ src/main/java/io/spring/image/demo/application/ImagesController.java
@@ -1,5 +1,8 @@
 package io.spring.image.demo.application;
 
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/upload")
 @Slf4j
-
+@RequiredArgsConstructor
 public class ImagesController {
+    private final ImageService service;
     //*
     // {"name": "", "size":100} //application/json
     //*
@@ -23,30 +27,39 @@ public class ImagesController {
     //*
 
     @PostMapping
-    public ResponseEntity uploadImage(@RequestParam("file") MultipartFile file,
+    public ResponseEntity save(@RequestParam("file") MultipartFile file,
                                       @RequestParam("name")String name,
                                       @RequestParam("tags") List<String> tags
     ) {
         log.info("Recebendo tentativa de upload do arquivo: {}", file.getOriginalFilename());
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags)) // ["tag1, "tag2"] -> "tag1, tag2"
+                .size(file.getSize())
+//                    .extension(file.getContentType()) //como vamos fazer isso? vamos imprimir no console através de nosso log.
+                .build();
 
-        try {
-            // Lógica de processamento...
-            if (file.isEmpty()) {
-                log.warn("O arquivo enviado estava vazio!");
-                return ResponseEntity.badRequest().body("Arquivo vazio");
-            }
-
-            log.info("Tamanho do arquivo recebido: {} bytes", file.getSize());
-            log.info("Nome definido para a imagem: {}", name);
-            log.info("Tags: {}", tags);
+        return ResponseEntity.ok().build();
 
 
-            return ResponseEntity.ok("Imagem enviada com Sucesso!!!!");
-        } catch (Exception e) {
-            // Sempre passe a exceção 'e' como último argumento para imprimir o StackTrace
-            log.error("Falha crítica ao processar imagem: ", e);
-            return ResponseEntity.internalServerError().body("Erro no servidor");
-        }
+//        try {
+//            // Lógica de processamento...
+//            if (file.isEmpty()) {
+//                log.warn("O arquivo enviado estava vazio!");
+//                return ResponseEntity.badRequest().body("Arquivo vazio");
+//            }
+//
+//            log.info("Tamanho do arquivo recebido: {} bytes", file.getSize());
+//            log.info("Nome definido para a imagem: {}", name);
+//            log.info("Tags: {}", tags);
+//
+//
+//            return ResponseEntity.ok("Imagem enviada com Sucesso!!!!");
+//        } catch (Exception e) {
+//            // Sempre passe a exceção 'e' como último argumento para imprimir o StackTrace
+//            log.error("Falha crítica ao processar imagem: ", e);
+//            return ResponseEntity.internalServerError().body("Erro no servidor");
+//        }
 
 
     }
